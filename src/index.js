045134c02c75159225359b1626570e16b2b4e84e
emirue <emirue.choi@i-on.net>
2019-12-26 14:24:33 +0900

    주석 추가...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import queryString from 'query-string';
 import _ from 'lodash';
 
 const index = {
+  /**
+   * Naver Cloud Platform 의 Cloud Functions 에서 실행되는 함수에서 받은 변수 params 를 파싱하는 함수
+   * @param params
+   * @returns {{headers: *, query: ParsedQuery}}
+   */
   parseParams(params) {
     const headers = params.__ow_headers;
     const query = queryString.parse(params.__ow_query);
