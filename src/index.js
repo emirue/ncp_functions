6cbc03575deb7d7300157cfbc884a92522d63814
emirue <emirue.choi@i-on.net>
2019-12-26 14:28:59 +0900

    함수 수정...

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ const index = {
   /**
    * Naver Cloud Platform 의 Cloud Functions 에서 실행되는 함수에서 받은 변수 params 를 파싱하는 함수
    * @param params
-   * @returns {{headers: *, query: ParsedQuery}}
+   * @returns {{headers: *, query: ParsedQuery, body: *}}
    */
   parseParams(params) {
+    const body = params.__ow_body;
     const headers = params.__ow_headers;
+    const method = params.__ow_method;
+    const path = params.__ow_path;
     const query = queryString.parse(params.__ow_query);
 
     return {
+      body,
       headers,
+      method,
+      path,
       query,
     }
   },
